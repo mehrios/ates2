53214701798edb9d2f4b01227dfad9ce82543db9
mehrios <66752268+mehrios@users.noreply.github.com>
2022-11-14 19:16:59 +0400

    Add files via upload

---
+++ ates2/script.js
@@ -0,0 +1,37 @@
+function gettime() {
+    let now = new Date();
+    return `${now.getHours()}:${now.getMinutes()}`
+}
+
+class Message {
+    constructor(author, message){
+        this.author = author;
+        this.time = gettime();
+        this.message = message;
+    }
+    toString(){
+        return `${this.time} ${this.author}: ${this.message}`;
+    }
+}
+
+
+class Messenger{
+    constructor(){
+        let mess=[];
+        this.mess=mess;
+    }
+    send(author, text){
+        const new_mess=new Message(author, text);
+        this.mess.push(new_mess);
+    }
+    show_history(){
+        this.mess.forEach(message => {
+            console.log(this.mess.toString());
+        });
+    }
+}
+
+let messenger = new Messenger()
+messenger.send('Артем', 'Первое сообщение')
+messenger.send('Мария', 'Второе сообщение')
+messenger.show_history()
